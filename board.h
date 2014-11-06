173ef7a04f21dfb38216ae23473b01ea9cc6c566
ojg <ojg@users.noreply.github.com>
2014-11-06 23:18:43 +0100

    Add clksys_driver (AVR1518) and switch clock to 32MHz

---
+++ board.h
@@ -0,0 +1,19 @@
+/*
+ * board.h
+ *
+ * Created: 06.11.2014 22:40:38
+ *  Author: ole
+ */ 
+
+
+#ifndef INCFILE1_H_
+#define INCFILE1_H_
+
+
+#define LED_PIN_bm PIN5_bm
+#define LED_PORT PORTD
+
+
+
+
+#endif /* INCFILE1_H_ */
--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 /*
- * octogain.c
+ * main.c
  *
  * Created: 06.11.2014 20:32:10
  *  Author: ole
  */ 
 
-#define F_CPU 2000000
+#define F_CPU 32000000
+
+#include "board.h"
+#include "clksys_driver.h"
 
 #include <avr/io.h>
 #include <util/delay.h>
 
 int main(void)
 {
-	PORTD.DIRSET = PIN5_bm;
+	LED_PORT.DIRSET = LED_PIN_bm;
+	
+	CLKSYS_Enable( OSC_RC32MEN_bm );
+	do {} while ( CLKSYS_IsReady( OSC_RC32MRDY_bm ) == 0 );
+	CLKSYS_Main_ClockSource_Select( CLK_SCLKSEL_RC32M_gc );
+	CLKSYS_Disable( OSC_RC2MEN_bm | OSC_RC32KEN_bm );
 	
 	while(1)
 	{
 		_delay_ms(500);  // Wait for 500ms
-		PORTD.OUTTGL = PIN5_bm;
+		LED_PORT.OUTTGL = LED_PIN_bm;		
 	}
 }
